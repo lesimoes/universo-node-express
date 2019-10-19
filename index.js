8b087f32c9b4e9d553d63144550c10d8229849ee
lesimoes <lesimoes0@gmail.com>
2019-10-18 22:01:36 -0300

    post

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 
 // Define uma rota com verbo GET
-app.get('/', async (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).send({data: 'Maarrr já?'})
 });
+// Define uma rota com o verbo POST
+app.post('/', (req, res) => {
+  res.status(200).send({data: 'Marr já em POST?'})
+})
 
 // Inicia o servidor na porta 3000
 app.listen(3000);
