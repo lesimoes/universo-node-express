738836bcfd5717113cc88baef60ca5f9a41d95ad
lesimoes <lesimoes0@gmail.com>
2019-10-18 22:10:06 -0300

    adiciona parametro e body na rota do POST

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+// Habilita a captura do body
+app.use(express.json({ strict: false, limit: '50mb' }));
+
 // Define uma rota com verbo GET
 // Pega 3 parametros na rota
 app.get('/:param1/:param2/:param3', (req, res) => {
@@ -13,8 +16,15 @@ app.get('/:param1/:param2/:param3', (req, res) => {
 });
 
 // Define uma rota com o verbo POST
-app.post('/', (req, res) => {
-  res.status(200).send({data: 'Marr já em POST?'})
+// Pega 1 parametro na rota
+// Pega o body em JSON
+app.post('/:param1', (req, res) => {
+  const { param1 } = req.params;
+  console.log(req.body)
+  res.status(200).send({
+    param1,
+    body: req.body,
+  })
 })
 
 // Inicia o servidor na porta 3000
